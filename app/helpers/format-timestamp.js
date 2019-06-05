961b86718c7422dab6fcf33a76ecc80dfd574a39
Dan Lubbers <lubbers.dan@gmail.com>
2019-06-04 20:18:01 -0400

    step4: helpers

---
+++ app/helpers/format-timestamp.js
@@ -0,0 +1,8 @@
+import { helper } from '@ember/component/helper';
+import { dateToString } from 'shlack/utils/date';
+
+export default helper(function formatTimestamp(params/*, hash*/) {
+  const [dateish] = params;
+  return dateToString(dateish)
+  
+});
